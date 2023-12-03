b4fefadf3c025dc1a685b8e64c99b1004f8cf9a0
raijinhasarrived <raijinhasarrived@gmail.com>
2023-12-03 20:38:12 +0500

    ⚡ input elem testing change event

--- src/components/raijin/raijin.test.js
+++ src/components/raijin/raijin.test.js
@@ -3,13 +3,23 @@ import { Raijin } from './raijin';
 
 it('testing button', () => {
   render(<Raijin />);
+
+  // Getting elemens from component
+
   const buttonElement = screen.getByText(/increment/i);
-
-  expect(buttonElement).toBeInTheDocument();
-
-  fireEvent.click(buttonElement);
-
+  const inputElement = screen.getByTestId('rainput');
   const countElement = screen.getByText(/we r testing btn/i);
 
+  expect(buttonElement).toBeInTheDocument();
+  expect(inputElement).toBeInTheDocument();
+
+  //Event firing
+
+  fireEvent.click(buttonElement);
+  fireEvent.change(inputElement, { target: { value: 'thunder hits everyday' } });
+
+  //Expected rusults
+
   expect(countElement).toHaveTextContent('We R Testing Btn 1');
+  expect(inputElement).toHaveValue('thunder hits everyday');
 });
