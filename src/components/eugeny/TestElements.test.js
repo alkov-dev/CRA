d3e489f6b7faeb481f0605dd5e8e6eadb0cb1906
alkovdev <darkzoner31@gmail.com>
2023-12-03 18:43:07 +0300

    new

--- src/components/eugeny/TestElements.test.js
+++ src/components/eugeny/TestElements.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react';
 import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
 import TestElements from './TestElements'
