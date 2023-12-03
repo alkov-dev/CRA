e80a4561bab881be2edfe9fa40862e6c74f95ecf
alkovdev <darkzoner31@gmail.com>
2023-12-03 18:41:09 +0300

    new tests

--- src/components/eugeny/TestElements.test.js
+++ src/components/eugeny/TestElements.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup, fireEvent, waitFor } from '@testing-library/react';
 import TestElements from './TestElements'
+import TestAsync from './TestAsync'
 
 afterEach(cleanup);
 
@@ -16,11 +17,11 @@ afterEach(cleanup);
     expect(getByTestId('button-up')).not.toHaveAttribute('disabled')
   });
 
-  it('should be disabled', () => {
-    const { getByTestId } = render(<TestElements />); 
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByTestId('button-down')).toBeDisabled()
-  });
+  // it('should be disabled', () => {
+  //   const { getByTestId } = render(<TestElements />); 
+  //   // eslint-disable-next-line testing-library/prefer-screen-queries
+  //   expect(getByTestId('button-down')).toBeDisabled()
+  // });
 
   it('increments counter', () => {
     const { getByTestId } = render(<TestElements />); 
@@ -30,4 +31,25 @@ afterEach(cleanup);
 
     // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByTestId('counter')).toHaveTextContent('1')
+  });
+
+  it('decrements counter', () => {
+    const { getByTestId } = render(<TestElements />); 
+    
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    fireEvent.click(getByTestId('button-down'))
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(getByTestId('counter')).toHaveTextContent('-1')
+  });
+
+  it('increments counter after 0.5s', async () => {
+    const { getByTestId, getByText } = render(<TestAsync />); 
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    fireEvent.click(getByTestId('button-up'))
+
+    const counter = await waitFor(() => getByText('1')) 
+
+    expect(counter).toHaveTextContent('1')
   });
