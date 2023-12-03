2ef4008f67363961fab86a34ad9aa9fddaad3411
alkovdev <darkzoner31@gmail.com>
2023-12-03 18:26:00 +0300

    new

--- src/components/eugeny/TestElements.test.js
+++ src/components/eugeny/TestElements.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, fireEvent } from '@testing-library/react';
 import TestElements from './TestElements'
 
 afterEach(cleanup);
@@ -9,3 +9,25 @@ afterEach(cleanup);
     // eslint-disable-next-line testing-library/prefer-screen-queries
     expect(getByTestId('counter')).toHaveTextContent(0)
    });
+
+   it('should be enabled', () => {
+    const { getByTestId } = render(<TestElements />);
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(getByTestId('button-up')).not.toHaveAttribute('disabled')
+  });
+
+  it('should be disabled', () => {
+    const { getByTestId } = render(<TestElements />); 
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(getByTestId('button-down')).toBeDisabled()
+  });
+
+  it('increments counter', () => {
+    const { getByTestId } = render(<TestElements />); 
+    
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    fireEvent.click(getByTestId('button-up'))
+
+    // eslint-disable-next-line testing-library/prefer-screen-queries
+    expect(getByTestId('counter')).toHaveTextContent('1')
+  });
